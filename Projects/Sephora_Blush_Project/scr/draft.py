6ff69f7007377ce70fb6a3f539a83bb6a201b6d4
thaopham98 <minhppham14@gmail.com>
2026-06-30 08:54:48 -0700

    rename folder

--- Projects/Sephora_Blush_Project/scr/draft.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-## Looking for the path
-print(f"Path Parent[0]: {Path(__file__).resolve().parents[0]}") # current parent
-print(f"Path Parent[1]: {Path(__file__).resolve().parents[1]}") # grandparent
-print(f"Path Parent[2]: {Path(__file__).resolve().parents[2]}") # grea-grandparent
